690e733390feb89e76e825bd87820e5650ebb68e
kmng1 <kmng@alumni.nus.edu.sg>
2017-06-16 17:14:00 +0800

    Images

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 //load libraries
 var express = require("express");
-var net = require("net");
-
+var fs = require('fs');
 
 //Create an instance of the express application
 var app = express();
 
+//Application Variables
+var imagesList = [];
+
 //Define a route to static resource
-app.get("/time", function(req, resp){
+app.use("Images",express.static(__dirname + "/Images"));
+app.use(express.static(__dirname + "/Public"));
+
+//Define response for request Index
+app.get("/Index", function(req, resp){
+
     console.log(" >>>  header: %s", req.get("User-Agent"));
     console.log(">>> method %s", req.header);
     console.log(">>> url: %s", req.url);
@@ -16,25 +23,41 @@ app.get("/time", function(req, resp){
 
     resp.status(202);
     resp.type("text/plain"); //MINE
-    resp.send("time: " + time);
+    resp.send("<h1>Yes This is the static Page</h1> \nTime: " + time);
+  
 }
 );
 
+//Define response for request randomImage
+app.get("/random", function(req, resp){
+    console.log(" >>>  header: %s", req.get("User-Agent"));
+    console.log(">>> method %s", req.header);
+    console.log(">>> url: %s", req.url);
 
+    
+}
+);
+
+//Read File in /Image
+var fs = require('fs');
+fs.realpath(__dirname, function(err, path) {
+    if (err) {
+        console.log(err);
+     return;
+    }
+    console.log('Path is : ' + path);
+});
+
+//list programs/files process/load
 for(var i in process.argv){
     console.log(">> argv = " + process.argv[i]
     + ", typeof = " + typeof(process.argv[i]));
 }
 
-//Define routes
-app.use(express.static(__dirname +"/public"));
-
-//Specify port that the app will be listening to
-//var port;
 var port = process.argv[2] || 3000;
-
 
 //bind the app to the port
 app.listen(port,function(){
     console.log("Application stated on port %d", port)
 });
+
